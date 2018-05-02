f681ae03f077e3305e841b92a68429392717ec8a
babazaroni <chuck@sanna.com>
2018-05-02 14:57:34 +0000

    video working

--- src/applications/detection/main.cpp
+++ src/applications/detection/main.cpp
@@ -400,7 +400,8 @@ int main(int argc, char** argv)
     // open video and check
     cv::VideoCapture video;
     if(useCamera)
-      video.open(std::atoi(cmdline._filename.c_str()));
+      video.open("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480, format=(string)I420, framerate=(fraction)24/1 ! nvvidconv flip-method=6 ! video/x-raw, format=(string)I420 ! videoconvert ! video/x-raw, format=(string)BGR ! appsink");
+//      video.open(std::atoi(cmdline._filename.c_str()));
     else
       video.open(cmdline._filename);
     
--- src/cctag/geometry/Distance.cpp
+++ src/cctag/geometry/Distance.cpp
@@ -5,7 +5,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-#include <immintrin.h>
+//#include <immintrin.h>
+#include "neon2sse.h"
 #include "Distance.hpp"
 
 namespace cctag {
